9612aed94daf5fb1d7ceeaf8c571caf2e1cf81be
FoulonNick <nick.foulon@student.howest.be>
2019-10-01 16:45:48 +0200

    added a list of organisers

--- DossierPDP/Models/Dossier.cs
+++ DossierPDP/Models/Dossier.cs
@@ -7,13 +7,15 @@ namespace DossierPDP.Models
 {
     public class Dossier
     {
-        public int IdDossier { get; set; }
-        public int CustomerID { get; set; }
-        public string Name { get; set; }
+        public int DossierId { get; set; }
+        public string DossierName { get; set; }
+        public List<Organiser> OrganiserList{ get; set; }
+        public int CustomerId { get; set; }
         public PartyType PartyType { get; set; }
         public DateTime DatePlanned { get; set; }
         public int Budget { get; set; }
         public Room Room { get; set; }
 
+        public List<>  { get; set; }
     }
 }

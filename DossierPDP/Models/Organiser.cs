9a3c71e038344d4400f72a05915421e177303a2a
Unknown <raf.gillisjans@student.howest.be>
2019-10-01 16:40:12 +0200

    Organiser object class

---
+++ DossierPDP/Models/Organiser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DossierPDP.Models
+{
+    public class Organiser
+    {
+        public int OrganiserId { get; set; }
+        public string OrganiserFirstName { get; set; }
+        public string OrganiserLastName { get; set; }
+        public string OrganiserAddress { get; set; }
+        public string OrganiserTelephoneNumber { get; set; }
+        public string FirstContact { get; set; }
+        public string OrganiserEmailAddress { get; set; }
+    }
+}

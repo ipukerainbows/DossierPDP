6a455262aee069fa860d58dbd41efe138815d5f1
Unknown <raf.gillisjans@student.howest.be>
2019-10-01 17:05:23 +0200

    Drinks object class

---
+++ DossierPDP/Models/Drinks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DossierPDP.Models
+{
+    public class Drinks
+    {
+        public int DrinkId { get; set; }
+        public string DrinkName { get; set; }
+    }
+}

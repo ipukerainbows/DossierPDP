cd45911243db43abb82aa2ac0f5ee453e13f67e2
FoulonNick <nick.foulon@student.howest.be>
2019-11-07 10:32:30 +0100

    Changed index layout
    I changed the layout of the index and added  a new button to create a dossier.

--- DossierPDP/Models/repositories/MockRepositories/DossierMockRepository.cs
+++ DossierPDP/Models/repositories/MockRepositories/DossierMockRepository.cs
@@ -14,7 +14,10 @@ namespace DossierPDP.Models.repositories
             {
                 new Dossier() { DossierId = 1, DossierName = "Dossier 1" , CustomerId = 1, DatePlanned = new DateTime(2017, 1, 1) },
                 new Dossier() { DossierId = 2, DossierName = "Dossier 2" , CustomerId = 2, DatePlanned = new DateTime(2017, 1, 2) },
-                new Dossier() { DossierId = 3, DossierName = "Dossier 3" , CustomerId = 2, DatePlanned = new DateTime(2017, 1, 3) }
+                new Dossier() { DossierId = 3, DossierName = "Dossier 3" , CustomerId = 2, DatePlanned = new DateTime(2017, 1, 3) },
+                new Dossier() { DossierId = 4, DossierName = "Dossier 4" , CustomerId = 1, DatePlanned = new DateTime(2017, 1, 1) },
+                new Dossier() { DossierId = 5, DossierName = "Dossier 5" , CustomerId = 2, DatePlanned = new DateTime(2017, 1, 2) },
+                new Dossier() { DossierId = 6, DossierName = "Dossier 6" , CustomerId = 2, DatePlanned = new DateTime(2017, 1, 3) }
             };
         }
         public IEnumerable<Dossier> GetAllDossier()

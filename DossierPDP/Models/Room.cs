73d7a3b51bea7c98ef6f919e477d835b923aeb5a
Unknown <raf.gillisjans@student.howest.be>
2019-11-07 12:27:41 +0100

    Room enum

--- DossierPDP/Models/Room.cs
+++ DossierPDP/Models/Room.cs
@@ -1,8 +1,16 @@
 ﻿namespace DossierPDP.Models
 {
-    public class Room
+    /*public class Room
     {
         public int RoomId { get; set; }
         public string RoomName { get; set; }
+    }*/
+    public enum Room
+    {
+        Tuin,
+        Terras,
+        Orangerie,
+        Middenzaal,
+        Straatkantzaal
     }
 }
